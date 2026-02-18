e2e781290fa78a257282c4189810fbab28d478bb
Sutou Kouhei <kou@clear-code.com>
2026-02-18 10:59:23 +0900

    Update version info to 1.2.7 (2026-02-18)

--- python/setup.py
+++ python/setup.py
@@ -50,7 +50,7 @@ class cmake_build_ext(build_ext):
 
 setup(
     name="openarm_can",
-    version="1.2.6",
+    version="1.2.7",
     license="Apache-2.0",
     license_files=["LICENSE.txt"],
     ext_modules=[Extension('__dummy__', [])],
